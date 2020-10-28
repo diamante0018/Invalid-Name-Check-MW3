6883cdb165ca1f28ef8bede91fc09b480897d50f
Edoardo Sanguineti <es597@sussex.ac.uk>
2020-10-28 15:26:57 +0100

    Remove weird strings from chat

--- InvalidName.cs
+++ InvalidName.cs
@@ -16,18 +16,29 @@ namespace InvalidName
             };
         }
 
+        public override EventEat OnSay3(Entity player, BaseScript.ChatType type, string name, ref string message)
+        {
+            if(!isNotControl(message))
+            {
+                Utilities.ExecuteCommand($"dropclient {player.EntRef} \"Lame Way to Crash Clients\"");
+                return EventEat.EatGame;
+            }
+
+            return EventEat.EatNone;
+        }
+
         public Boolean canConnect(string Name)
         {
-            if (string.IsNullOrWhiteSpace(Name) || Name.Replace(" ", "").Length < 3)
+            if (string.IsNullOrWhiteSpace(Name) || Name.Replace(" ", "").Length < 4)
             {
                 return false;
             }
             return isNotControl(Name);
         }
 
-        public Boolean isNotControl(string Name)
+        public Boolean isNotControl(string Word)
         {
-            foreach(Char c in Name) {
+            foreach(Char c in Word) {
                 if (Char.IsControl(c))
                     return false;
             }
