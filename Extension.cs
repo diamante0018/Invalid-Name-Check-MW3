f59e49cdaf901fef19c5525d50bdf8d3871aaa24
Edoardo Sanguineti <es597@sussex.ac.uk>
2020-10-31 22:34:43 +0100

    Regex Way

---
+++ Extension.cs
@@ -0,0 +1,49 @@
+﻿using InfinityScript;
+using System.Text;
+
+namespace ClanTagExt
+{
+    public static class ExtTag
+    {
+        public static unsafe void SetClanTag(this Entity player, string tag)
+        {
+            if (player == null || !player.IsPlayer || tag.Length > 8)
+                return;
+
+            int address = 0x38A4 * player.EntRef + 0x01AC5564;
+
+            for (int i = 0; i < tag.Length; i++)
+                *(byte*)(address + i) = (byte)tag[i];
+
+            *(byte*)(address + tag.Length) = 0;
+        }
+
+        public static unsafe string GetClanTag(this Entity player)
+        {
+            if (player == null || !player.IsPlayer)
+                return null;
+
+            int address = 0x38A4 * player.EntRef + 0x01AC5564;
+
+            string result = "";
+            for (; address < address + 8 && *(byte*)address != 0; address++)
+                result += Encoding.ASCII.GetString(new byte[] { *(byte*)address });
+
+            return result;
+        }
+
+        public static unsafe string setName(this Entity player, string name)
+        {
+            if (player == null || !player.IsPlayer || name.Length > 15)
+                return null;
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                *(byte*)((0x38A4 * player.EntRef + 0x1AC5490) + i) = (byte)name[i];
+                *(byte*)((0x38A4 * player.EntRef + 0x1AC5508) + i) = (byte)name[i];
+            }
+
+            return name;
+        }
+    }
+}
--- InvalidName.cs
+++ InvalidName.cs
@@ -1,24 +1,44 @@
-﻿using InfinityScript;
-using System;
+﻿using ClanTagExt;
+using InfinityScript;
+using System.Text.RegularExpressions;
 
 namespace InvalidName
 {
     class InvalidName : BaseScript
     {
+        private Regex rx = new Regex(@"^[\x00-\x7F]+$");
         public InvalidName()
         { 
             base.PlayerConnected += delegate (Entity player)
             {
-                if(!canConnect(player.Name))
+                if(!(RegexWay(player.Name) && CheckClanTag(player)))
                 {
                     Utilities.ExecuteCommand($"dropclient {player.EntRef} \"Invalid Name Buddy\"");
                 }
+
+                CheckLength(player);
             };
+        }
+
+        public bool CheckClanTag(Entity player)
+        {
+            if (string.IsNullOrEmpty(player.GetClanTag()))
+                return true;
+
+            return RegexWay(player.GetClanTag().Replace("^", ""));
+        }
+
+        public void CheckLength(Entity player)
+        {
+            if (string.IsNullOrWhiteSpace(player.Name) || player.Name.Replace(" ", "").Length < 4)
+            {
+                Utilities.ExecuteCommand($"dropclient {player.EntRef} \"Your Name is Too Short\"");
+            }
         }
 
         public override EventEat OnSay3(Entity player, BaseScript.ChatType type, string name, ref string message)
         {
-            if(!isNotControl(message))
+            if(RegexWay(message.Replace("^", "")))
             {
                 Utilities.ExecuteCommand($"dropclient {player.EntRef} \"Lame Way to Crash Clients\"");
                 return EventEat.EatGame;
@@ -27,22 +47,9 @@ namespace InvalidName
             return EventEat.EatNone;
         }
 
-        public Boolean canConnect(string Name)
+        public bool RegexWay(string input)
         {
-            if (string.IsNullOrWhiteSpace(Name) || Name.Replace(" ", "").Length < 4)
-            {
-                return false;
-            }
-            return isNotControl(Name);
-        }
-
-        public Boolean isNotControl(string Word)
-        {
-            foreach(Char c in Word) {
-                if (Char.IsControl(c))
-                    return false;
-            }
-            return true;
+            return rx.IsMatch(input);
         }
     }
 }
